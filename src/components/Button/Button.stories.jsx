529c9917fb12c1fca2f759a8e9fc11f7ff0dd06a
Katariina Kaunisto <katariina.kaunisto@hotmail.se>
2024-10-05 16:31:20 +0200

    Button created

--- src/components/Button/Button.stories.jsx
+++ src/components/Button/Button.stories.jsx
@@ -1,18 +1,21 @@
 import Button from "./Button";
+import {action} from "@storybook/addon-actions";
 
 export default {
-  title: "taskmaster/components/Button",
+  title: "katkaun-components/components/Button",
   component: Button,
 };
 
 export const Default = {
   args: {
     title: "Click me!",
+    onClick: action("button-clicked"),  
   },
 };
 
 export const NoTitle = {
   args: {
     title: "Default",
+    onClick: action("button-clicked"),  
   },
 };
